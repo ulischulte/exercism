350adbad5124a18d608abe6b1f46b71c53bf3672
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-07 20:05:55 +0200

    Adjusted 'Trinagle' solution

--- kotlin/triangle/src/main/kotlin/Triangle.kt
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,15 +1,15 @@
-class Triangle<out T : Number>(private val a: T, private val b: T, private val c: T) {
-    init {
-        val sides = listOf(a, b, c)
-            .map(Number::toDouble)
-            .sorted()
+class Triangle<out T : Number>(a: T, b: T, c: T) {
+    private val sides = listOf(a, b, c)
+        .map(Number::toDouble)
+        .sorted()
 
+    init {
         require(sides[0] > 0 && sides[0] + sides[1] > sides[2]) {
             "not a triangle"
         }
     }
 
-    val isEquilateral: Boolean get() = a == b && b == c
-    val isIsosceles: Boolean get() = a == b || b == c || a == c
+    val isEquilateral: Boolean get() = sides[0] == sides[1] && sides[1] == sides[2]
+    val isIsosceles: Boolean get() = sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2]
     val isScalene: Boolean get() = !isIsosceles
 }
