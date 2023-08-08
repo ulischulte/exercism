7ff3e1feefcf84e66e62303182fe914e6e34ecfc
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-08 09:27:21 +0200

    Formatting

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -2,17 +2,19 @@ data class MatrixCoordinate(val rowIndex: Int, val columnIndex: Int)
 
 class Matrix(private val grid: List<List<Int>>) {
 
-	val saddlePoints: Set<MatrixCoordinate> get() = calculateSaddlePoints()
+  val saddlePoints: Set<MatrixCoordinate> get() = calculateSaddlePoints()
 
-	private fun calculateSaddlePoints(): Set<MatrixCoordinate> {
-		return grid.mapIndexed { rowIndex, row ->
-			row.mapIndexedNotNull { columnIndex, _ ->
-				val maxInRow = row.maxOrNull()
-				val minInColumn = grid.minOfOrNull { it[columnIndex] }
-				if (maxInRow == minInColumn)
-					MatrixCoordinate(rowIndex + 1, columnIndex + 1)
-				else null
-			}
-		}.flatten().toSet()
-	}
+  private fun calculateSaddlePoints(): Set<MatrixCoordinate> {
+    return grid.flatMapIndexed { rowIndex, row ->
+      row.mapIndexedNotNull { columnIndex, _ ->
+        val maxInRow = row.maxOrNull()
+        val minInColumn = grid.minOfOrNull { it[columnIndex] }
+        if (maxInRow == minInColumn) {
+          MatrixCoordinate(rowIndex + 1, columnIndex + 1)
+        } else {
+          null
+        }
+      }
+    }.toSet()
+  }
 }
