0983b33c5e8f7169ccac22b4ec87d22a85e3601d
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-10 10:13:52 +0200

    improved solution for rotational-cipher

--- kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,22 +1,18 @@
-class RotationalCipher(key: Int) {
-  private val shiftKey: Int
+class RotationalCipher(private val shiftKey: Int) {
 
   init {
-    require(key in 0..26) { "Key must be between 0 and 26" }
-    shiftKey = key
+    require(shiftKey in 0..26) { "Key must be between 0 and 26" }
   }
 
-  fun encode(text: String): String {
-    return text.map { char ->
-      when {
-        char.isUpperCase() -> rotate(char, 'A')
-        char.isLowerCase() -> rotate(char, 'a')
-        else -> char
-      }
-    }.joinToString("")
-  }
+  fun encode(text: String): String =
+      text.map { char ->
+        when {
+          char.isUpperCase() -> rotate(char, 'A')
+          char.isLowerCase() -> rotate(char, 'a')
+          else -> char
+        }
+      }.joinToString("")
 
-  private fun rotate(c: Char, zeroPoint: Char): Char {
-    return ((c - zeroPoint + shiftKey) % 26 + zeroPoint.code).toChar()
-  }
+  private fun rotate(character: Char, zeroPoint: Char): Char =
+      ((character - zeroPoint + shiftKey) % 26 + zeroPoint.code).toChar()
 }
