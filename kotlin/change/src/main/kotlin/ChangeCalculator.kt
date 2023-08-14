aa6831cfb97aa0c6da9a2776bdc17a1f18f221fb
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-14 09:41:10 +0200

    Improved solution for change

--- kotlin/change/src/main/kotlin/ChangeCalculator.kt
+++ kotlin/change/src/main/kotlin/ChangeCalculator.kt
@@ -1,26 +1,34 @@
 class ChangeCalculator(private val coins: List<Int>) {
 
-  fun computeMostEfficientChange(change: Int): List<Int> {
-    require(change >= 0) { "Negative totals are not allowed." }
-    if (change <= 0) return listOf()
-    return computeChange(change).sorted()
+  init {
+    require(coins.all { it > 0 }) { "Coin values should be positive." }
   }
 
-  private fun computeChange(change: Int): List<Int> {
-    val changes = MutableList(change + 1) { emptyList<Int>() }
-    for (i in 0..change) {
-      val minChange = computeMinChange(i, changes)
-      changes[i] = if (minChange.sum() == i) minChange else emptyList()
-    }
-    return changes[change].takeIf { it.sum() == change }
+  fun computeMostEfficientChange(change: Int): List<Int> {
+    require(change >= 0) { "Negative totals are not allowed." }
+
+    if (change == 0) return listOf()
+
+    val sortedCoins = coins.sortedDescending()
+    return computeChange(sortedCoins, change)
       ?: throw IllegalArgumentException("The total $change cannot be represented in the given currency.")
   }
 
-  private fun computeMinChange(change: Int, lowerChanges: List<List<Int>>): List<Int> {
-    return coins.filter { it <= change }
-      .reversed()
-      .map { listOf(it) + lowerChanges[change - it] }
-      .filter { it.sum() == change }
-      .minByOrNull { it.size } ?: emptyList()
+  private fun computeChange(sortedCoins: List<Int>, change: Int): List<Int>? {
+    val coinsUsed = Array(change + 1) { Int.MAX_VALUE }
+    coinsUsed[0] = 0
+
+    val changeCombinations = Array(change + 1) { mutableListOf<Int>() }
+
+    for (coin in sortedCoins) {
+      for (i in coin..change) {
+        if (coinsUsed[i - coin] != Int.MAX_VALUE && coinsUsed[i - coin] + 1 < coinsUsed[i]) {
+          coinsUsed[i] = coinsUsed[i - coin] + 1
+          changeCombinations[i] = (changeCombinations[i - coin] + coin).toMutableList()
+        }
+      }
+    }
+
+    return changeCombinations.last().takeIf { it.sum() == change }
   }
 }
