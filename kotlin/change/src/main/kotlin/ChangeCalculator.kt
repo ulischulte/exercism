0f5582a46c1aa54f2337251cb2fefeeb3a268073
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-14 11:19:31 +0200

    Improved solution for change

--- kotlin/change/src/main/kotlin/ChangeCalculator.kt
+++ kotlin/change/src/main/kotlin/ChangeCalculator.kt
@@ -1,7 +1,9 @@
 class ChangeCalculator(private val coins: List<Int>) {
 
+  private val sortedCoins: List<Int> = coins.sortedDescending()
+
   init {
-    require(coins.all { it > 0 }) { "Coin values should be positive." }
+    require(sortedCoins.all { it > 0 }) { "Coin values should be positive." }
   }
 
   fun computeMostEfficientChange(change: Int): List<Int> {
@@ -9,22 +11,21 @@ class ChangeCalculator(private val coins: List<Int>) {
 
     if (change == 0) return listOf()
 
-    val sortedCoins = coins.sortedDescending()
     return computeChange(sortedCoins, change)
       ?: throw IllegalArgumentException("The total $change cannot be represented in the given currency.")
   }
 
   private fun computeChange(sortedCoins: List<Int>, change: Int): List<Int>? {
-    val coinsUsed = Array(change + 1) { Int.MAX_VALUE }
-    coinsUsed[0] = 0
+    val sortedCoinsUsed = Array(change + 1) { Int.MAX_VALUE }
+    sortedCoinsUsed[0] = 0
 
     val changeCombinations = Array(change + 1) { mutableListOf<Int>() }
 
     for (coin in sortedCoins) {
-      for (i in coin..change) {
-        if (coinsUsed[i - coin] != Int.MAX_VALUE && coinsUsed[i - coin] + 1 < coinsUsed[i]) {
-          coinsUsed[i] = coinsUsed[i - coin] + 1
-          changeCombinations[i] = (changeCombinations[i - coin] + coin).toMutableList()
+      for (changeAmount in coin..change) {
+        if (sortedCoinsUsed[changeAmount - coin] != Int.MAX_VALUE && sortedCoinsUsed[changeAmount - coin] + 1 < sortedCoinsUsed[changeAmount]) {
+          sortedCoinsUsed[changeAmount] = sortedCoinsUsed[changeAmount - coin] + 1
+          changeCombinations[changeAmount] = (changeCombinations[changeAmount - coin] + coin).toMutableList()
         }
       }
     }
