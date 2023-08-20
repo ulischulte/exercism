bb3728fe32986ebdfaf4e6bee4960bdb9bd31386
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-20 20:31:56 +0200

    Improved solution for pig-latin

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,20 +1,23 @@
 object PigLatin {
 
-  private val twoLetterClusters = setOf(
+  private val TWO_LETTER_CLUSTERS = setOf(
     "bl", "br", "ch", "cl", "cr", "dr", "fl", "fr", "gl", "gr", "pl", "pr",
     "qu", "rh", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th",
     "tr", "tw", "wh", "wr"
   )
-  private val threeLetterClusters = setOf("spl", "spr", "str", "scr", "shr", "squ", "thr", "sch")
 
-  fun translate(phrase: String) =
+  private val THREE_LETTER_CLUSTERS = setOf("spl", "spr", "str", "scr", "shr", "squ", "thr", "sch")
+
+  private val VOWEL_START_REGEX = Regex("^([aeiou]|xr|yt).*")
+
+  fun translate(phrase: String): String =
     phrase.split(" ").joinToString(" ") { applyPigLatin(it) }
 
   private fun applyPigLatin(word: String): String {
     val start = when {
-      word.matches(Regex("^([aeiou]|xr|yt).*")) -> 0
-      word.take(3) in threeLetterClusters -> 3
-      word.take(2) in twoLetterClusters -> 2
+      word.matches(VOWEL_START_REGEX) -> 0
+      word.take(3) in THREE_LETTER_CLUSTERS -> 3
+      word.take(2) in TWO_LETTER_CLUSTERS -> 2
       else -> 1
     }
     return "${word.drop(start)}${word.take(start)}ay"
