01f8e208096559332d410499be1e453c3e65e80a
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-20 21:26:29 +0200

    Improved solution for angram

--- kotlin/anagram/src/main/kotlin/Anagram.kt
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -1,17 +1,19 @@
 class Anagram(private val anagram: String) {
+  private val charCounts = anagram.lowercase().charsCount()
 
   fun match(anagrams: Collection<String>): Set<String> {
     return anagrams.filter {
       it.isAnagram(this.anagram.lowercase())
-    }
-      .toSet()
+    }.toSet()
   }
 
   private fun String.isAnagram(anagram: String): Boolean {
-    val anagramChars = anagram.lowercase().toMutableList()
-    this.lowercase().toList().forEach { anagramChar: Char ->
-      anagramChars.remove(anagramChar)
-    }
-    return anagramChars.isEmpty() && this.length == anagram.length && this.lowercase() != anagram
+    return this.length == anagram.length &&
+        this.lowercase() != anagram &&
+        this.lowercase().charsCount() == charCounts
+  }
+
+  private fun String.charsCount() : Map<Char, Int>{
+    return this.groupingBy { it }.eachCount()
   }
 }
