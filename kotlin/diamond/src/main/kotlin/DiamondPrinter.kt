45b55fdab10c6fc79214fb3ae353ef328c1d8d5b
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-22 18:46:39 +0200

    Added solution for diamond

---
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -0,0 +1,19 @@
+class DiamondPrinter {
+  private val alphabet = 'A'..'Z'
+
+  fun printToList(input: Char): List<String> {
+    val size = alphabet.indexOf(input) + 1
+    val upHalf = (0 until size).map { createDiamondLine(it, size) }
+    val downHalf = upHalf.dropLast(1).reversed()
+    return upHalf + downHalf
+  }
+
+  private fun createDiamondLine(index: Int, size: Int): String = with(alphabet.elementAt(index)) {
+    if (index == 0)
+      "${pad(size - 1)}$this${pad(size - 1)}"
+    else
+      "${pad(size - index - 1)}$this${pad(index * 2 - 1)}$this${pad(size - index - 1)}"
+  }
+
+  private fun pad(size: Int): String = " ".repeat(size)
+}
