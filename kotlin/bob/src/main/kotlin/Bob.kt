e20f6b2c79f7c17d2497d9a24c4daede4094cb75
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-25 16:44:45 +0200

    Improved solution for Bob

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,12 +1,13 @@
 object Bob {
-    private const val SHOUTING = "(?=.*[A-Z])(?!^\\s*\$)^[^a-z]+\$"
-    private const val SHOUTING_QUESTION = "^(?=.*[A-Z])[A-Z\\s]*\\?$"
+  private const val SHOUTING = "(?=.*[A-Z])(^[A-Z\\d\\s,!:^%@'#*&$()?]*)"
 
-    fun hey(input: String): String = when {
-        input.matches(Regex(SHOUTING_QUESTION)) -> "Calm down, I know what I'm doing!"
-        input.trim().endsWith("?") -> "Sure."
-        input.matches(Regex(SHOUTING)) -> "Whoa, chill out!"
-        input.isBlank() -> "Fine. Be that way!"
-        else -> "Whatever."
-    }
+  fun hey(input: String): String = when {
+    input.isBlank() -> "Fine. Be that way!"
+    input.trim().matches(Regex(SHOUTING)) && input.isQuestion() -> "Calm down, I know what I'm doing!"
+    input.isQuestion() -> "Sure."
+    input.matches(Regex(SHOUTING)) -> "Whoa, chill out!"
+    else -> "Whatever."
+  }
+
+  private fun String.isQuestion(): Boolean = this.trim().endsWith("?")
 }
