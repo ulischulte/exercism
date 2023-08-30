f3c1ecc1b09ce316a4220de04e190b05e8adda7f
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-30 18:37:57 +0200

    Improved solution for resistor-color-trio

--- kotlin/resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ kotlin/resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -2,22 +2,21 @@ import kotlin.math.pow
 
 object ResistorColorTrio {
 
-  private fun value(colorValue: List<Int>): Int {
-    return colorValue.take(2)
-      .reduceIndexed { index, color, acc -> (color * 10.0.pow(index).toInt()) + acc }
-  }
+  private fun value(colorValue: List<Int>): Int =
+    colorValue.take(2).reduceIndexed { _, acc, color -> acc * 10 + color }
 
   fun text(vararg input: Color): String {
     require(input.size >= 2)
     val colorOrdinals = input.map(Color::ordinal)
     val baseValue = value(colorOrdinals) * 10.0.pow(colorOrdinals.last()).toInt()
-    return with(baseValue) {
-      when {
-        this >= 1_000_000 -> "${this / 1_000_000} megaohms"
-        this >= 1_000 -> "${this / 1_000} kiloohms"
-        else -> "$this ohms"
-      }
-    }
-  }
-}
 
+    return formatOutput(baseValue)
+  }
+
+  private fun formatOutput(baseValue: Int): String =
+    when {
+      baseValue >= 1_000_000 -> "${baseValue / 1_000_000} megaohms"
+      baseValue >= 1_000 -> "${baseValue / 1_000} kiloohms"
+      else -> "$baseValue ohms"
+    }
+}
