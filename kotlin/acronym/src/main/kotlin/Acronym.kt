fdcda7612a83fb558477146a079184224992dc54
ulrichschulte <ulrich.schulte@codecentric.de>
2023-08-30 17:32:27 +0200

    Shortened solution for acronym

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -2,5 +2,5 @@ object Acronym {
   fun generate(phrase: String): String = phrase
     .split(Regex("\\s|[-_]"))
     .mapNotNull { it.firstOrNull()?.uppercase() }
-    .joinToString(separator = "")
+    .joinToString("")
 }
