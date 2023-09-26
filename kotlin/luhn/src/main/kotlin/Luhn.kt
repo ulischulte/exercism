c3d6b5f6b95e4c8376a655718c6ca624a4d93042
ulrichschulte <ulrich.schulte@codecentric.de>
2023-09-26 08:17:53 +0200

    Improved solution for luhn

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,27 +1,33 @@
 object Luhn {
-  fun isValid(candidate: String): Boolean {
-    if (!candidate.matches(Regex("^[\\d\\s]*$"))) return false
-    val normalizedCandidate = candidate.normalizeCandidate()
-    return isValidLength(normalizedCandidate) && isValidFormat(normalizedCandidate) && isValidCheckSum(normalizedCandidate)
+
+  private val DIGITS_AND_SPACES = Regex("^[\\d\\s]*$")
+
+  fun isValid(candidate: String): Boolean = with(candidate.normalizeCandidate()) {
+    isDigitsAndSpaces(candidate)
+        && isValidLength()
+        && isValidFormat()
+        && isValidCheckSum()
   }
 
-  private fun isValidLength(candidate: String) = candidate.length > 1
+  private fun String.normalizeCandidate(): String = this.filter { it.isDigit() }
 
-  private fun isValidFormat(candidate: String) = candidate.all { it.isDigit() }
+  private fun isDigitsAndSpaces(candidate: String): Boolean = candidate.matches(DIGITS_AND_SPACES)
 
-  private fun isValidCheckSum(candidate: String): Boolean {
-    val digits = candidate.map { it.toString().toInt() }
-    val checkSum = digits.mapIndexed { index, digit ->
-      if ((digits.size - index) % 2 == 0) doubleWithSubtraction(digit) else digit
-    }.sum()
+  private fun String.isValidLength(): Boolean = this.length > 1
+
+  private fun String.isValidFormat(): Boolean = this.all { it.isDigit() }
+
+  private fun String.isValidCheckSum(): Boolean {
+    val checkSum = this.map { it.toString().toInt() }
+      .mapIndexed { index, digit ->
+        if ((this.length - index) % 2 == 0) digit.doubleWithSubtraction()
+        else digit
+      }.sum()
     return checkSum % 10 == 0
   }
 
-  private fun doubleWithSubtraction(digit: Int): Int {
-    val doubled = digit * 2
-    return if (doubled > 9) doubled - 9
-    else doubled
+  private fun Int.doubleWithSubtraction() : Int = when (val doubled = this * 2) {
+    in 0..9 -> doubled  // Return doubled if it's less than 9
+    else -> doubled - 9 // Subtract 9 otherwise
   }
-
-  private fun String.normalizeCandidate() = this.filter { it.isDigit() }
 }
