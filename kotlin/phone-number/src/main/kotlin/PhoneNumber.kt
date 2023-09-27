4e56a8aa4b7d975c33e2d70b6f894682204f6142
ulrichschulte <ulrich.schulte@codecentric.de>
2023-09-27 18:19:10 +0200

    Improved solution for phone-number

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -2,29 +2,15 @@ class PhoneNumber(numberString: String) {
   val number: String
 
   init {
-    val digitsFromNumberString = numberString.filter { it.isDigit() }
-    val nanpNumber = digitsFromNumberString.let {
+    val nanpNumber = numberString.filter { it.isDigit() }.let {
       if (it.length == 11 && it.first() == '1') it.drop(1)
       else it
     }
-    validateNumberLength(nanpNumber)
-    validateFirstDigit(nanpNumber)
-    validateAreaCode(nanpNumber)
-    validateOnlyValidCharsArePresent(nanpNumber)
+    require(nanpNumber.length == 10)
+    require(nanpNumber.first().digitToInt() > 1)
+    require(nanpNumber[3].digitToInt() > 1)
+    require(nanpNumber.matches(Regex("[^\\p{L}\\p{P}]*")))
 
     number = nanpNumber
   }
-
-  private fun validateNumberLength(number: String) = require(number.length in arrayOf(10, 11))
-
-  private fun validateFirstDigit(number: String) =
-    if (number.length == 10) {
-      require(number.first().digitToInt() > 1)
-    } else {
-      require(number.first().digitToInt() == 1)
-    }
-
-  private fun validateAreaCode(number: String) = require(number[number.length - 7].digitToInt() > 1)
-
-  private fun validateOnlyValidCharsArePresent(number: String) = require(number.matches(Regex("[^\\p{L}\\p{P}]*")))
 }
