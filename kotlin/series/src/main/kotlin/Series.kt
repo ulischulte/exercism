92ac1bed05333d80e85cea3b329f9c47957c1869
ulrichschulte <ulrich.schulte@codecentric.de>
2023-10-09 08:33:50 +0200

    Improved solution for series

--- kotlin/series/src/main/kotlin/Series.kt
+++ kotlin/series/src/main/kotlin/Series.kt
@@ -1,8 +1,7 @@
 object Series {
   fun slices(lengthOfSlices: Int, digitsString: String): List<List<Int>> {
     validateInput(lengthOfSlices, digitsString)
-    val sliceIndicesRange = 0 until (digitsString.length - lengthOfSlices + 1)
-    return sliceIndicesRange.map { index -> sliceToDigitList(digitsString, index, lengthOfSlices) }
+    return digitsString.map { it.toString().toInt() }.windowed(lengthOfSlices)
   }
 
   private fun validateInput(lengthOfSlices: Int, digitsString: String) {
@@ -14,9 +13,4 @@ object Series {
     )
   }
 
-  private fun sliceToDigitList(digitsString: String, startIndex: Int, lengthOfSlices: Int): List<Int> {
-    return digitsString.substring(startIndex until startIndex + lengthOfSlices).toIntList()
-  }
-
-  private fun String.toIntList(): List<Int> = this.map { it.digitToInt() }
 }
